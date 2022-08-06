0d29e6b60b961d23fb8188a7e360e2a122fdda95
kumabio <kumabio.prince@student.moringaschool.com>
2022-08-06 22:34:07 +0000

    Assignment Completed

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ getFormData }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -20,7 +20,23 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+    const newFormData = {
+      prompt: formData.prompt,
+      answers: [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+      correctIndex: formData.correctIndex
+    }
+    setFormData(newFormData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newFormData),
+    })
+      .then((r) => r.json())
+      .then((data) => getFormData(data))
   }
+
 
   return (
     <section>
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,12 @@
-import React from "react";
+import QuestionItem from './QuestionItem'
 
-function QuestionList() {
+
+function QuestionList({ qLists, onDeleteItem, onUpdateItem }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{qLists.map((qList) => <QuestionItem key={qList.id} question={qList} onDeleteItem={onDeleteItem} onUpdateItem={onUpdateItem} />)
+      }</ul>
     </section>
   );
 }
